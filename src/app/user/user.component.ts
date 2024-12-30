2d0f6d40a4af9a05f209783f6e4b4a7840919af7
Mingyang <limingyanglmy10@gmail.com>
2024-12-31 00:44:27 +0800

    Add users

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-
+  selectedUser = DUMMY_USERS[0];
 }
