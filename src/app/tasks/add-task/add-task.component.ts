46926faffe4bdd9646c016e489669de35f3f881e
Mingyang <limingyanglmy10@gmail.com>
2025-01-03 23:30:14 +0800

    Add basic components

---
+++ src/app/tasks/add-task/add-task.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-task.component.html',
+  styleUrl: './add-task.component.css',
+})
+export class AddTaskComponent {}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,24 @@
-import { Component } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import {
+  Component,
+  computed,
+  EventEmitter,
+  Input,
+  input,
+  Output,
+} from '@angular/core';
+import { type User } from './user.model';
+
+// type User = {
+//   id: string;
+//   name: string;
+//   avatar: string;
+// };
+
+// interface User {
+//   id: string;
+//   name: string;
+//   avatar: string;
+// }
 
 @Component({
   selector: 'app-user',
@@ -9,5 +28,22 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[0];
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  // @Input({ required: true }) id!: string;
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  // id = input.required<string>();
+  //imagePath = computed(() => 'assets/users/' + this.avatar());
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
+  @Output()
+  select = new EventEmitter<string>();
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
 }
